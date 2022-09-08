0c23a2eee08f73a307fabda37f988ec9dd9457e7
Richard Niemand <niemand.richard@gmail.com>
2022-09-07 19:12:58 -0600

    Testing InfluxDB output

--- src/DevConsole/Program.cs
+++ src/DevConsole/Program.cs
@@ -1,16 +1,20 @@
+using DevConsole;
 using DevConsole.Builders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RnCore.Metrics;
 using RnCore.Metrics.Extensions;
+using RnCore.Metrics.Outputs;
 
 IConfigurationRoot config = new ConfigurationBuilder()
   .AddJsonFile("appsettings.json")
+  .AddJsonFile("appsettings.machine.json", optional: true)
   .Build();
 
 ServiceProvider serviceProvider = new ServiceCollection()
   .AddSingleton<IConfiguration>(config)
   .AddRnCoreMetrics()
+  .AddSingleton<IMetricOutput, InfluxDbOutput>()
   .BuildServiceProvider();
 
 var metricsService = serviceProvider.GetRequiredService<IMetricsService>();
